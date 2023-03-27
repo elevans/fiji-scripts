8ae6274a0a625041b9bbcc129cd4367906f0e0d0
Edward Evans <elevans2@wisc.edu>
2023-03-27 17:46:25 -0500

    Add compute_states function

--- python/threshold_measure.py
+++ python/threshold_measure.py
@@ -7,7 +7,10 @@
 
 import ij.IJ as IJ
 import ij.plugin.frame.RoiManager as RoiManager
+from ij.measure import ResultsTable
+from collections import defaultdict
 from net.imglib2.algorithm.labeling.ConnectedComponents import StructuringElement
+from net.imglib2.roi import Regions
 from net.imglib2.roi.labeling import LabelRegions
 
 def get_roi_manager():
@@ -20,24 +23,52 @@ def get_region_labels(regions):
     return list(regions.getExistingLabels())
 
 
-def apply_threshold(method):
-    h = ops.run("image.histogram", data)
+def apply_threshold(method, img):
+    h = ops.run("image.histogram", img)
     t_val = ops.run("threshold.{}".format(method), h)
-    t_out = ops.run("threshold.apply", data, t_val)
+    t_out = ops.run("threshold.apply", img, t_val)
     return t_out
 
 
 def run_cca(img, add_to_roi_manager=True):
-    img_l = ops.run("cca", img, StructuringElement.EIGHT_CONNECTED)
-    return LabelRegions(img_l)
+    return ops.run("cca", img, StructuringElement.EIGHT_CONNECTED)
 
 
-def compute_stats(regions, img, stats):
-    return
+def compute_stats(labeling, img):
+    regions = LabelRegions(labeling)
+    for r in regions:
+        stats = defaultdict(list)
+        samples = Regions.sample(r, img)
+        stats["area"].append(ops.run("stats.size", samples).getRealDouble())
+        stats["mean"].append(ops.run("stats.mean", samples).getRealDouble())
+        min_max = ops.run("stats.minMax", samples)
+        stats["min"].append(min_max.getA().getRealDouble())
+        stats["max"].append(min_max.getB().getRealDouble())
+
+    # create an empty table
+    table = ResultsTable()
+
+    # add columns to the table
+    for key in stats.keys():
+        table.setHeading(key)
+    
+    # add rows to the table
+    num_rows = max([len](x) for x in stats.values())
+    for i in range(num_rows):
+        row = []
+        for key in stats.keys():
+            if i < len(stats[key]):
+                row.append(stats[key][i])
+            else:
+                row.append('')
+        table.addRow(row)
+    
+    return table
 
 get_roi_manager()
-mask = apply_threshold(method)
+mask = apply_threshold(method, data)
 ui.show("mask", mask)
-regions = run_cca(mask)
-labels = get_region_labels(regions)
+labeling = run_cca(mask)
+results_table = compute_stats(labeling, data)
+results_table.show('Results')
 print("Done!")

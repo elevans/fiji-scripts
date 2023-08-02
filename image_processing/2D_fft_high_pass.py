717c849f681c52061e89c778e89971bd150f0d57
Edward Evans <elevans2@wisc.edu>
2023-08-02 13:41:10 -0500

    Add FFT high-pass filter scripts

---
+++ image_processing/2D_fft_high_pass.py
@@ -0,0 +1,52 @@
+#@ ImgPlus img
+#@ OpService ops
+#@ UIService ui
+#@ Integer radius(label="Radius:", value=0)
+#@output ImgPlus recon
+
+from net.imglib2.img.display.imagej import ImageJFunctions
+from net.imglib2.util import Util
+
+# run FFT on the input image
+fft_img = ops.filter().fft(img)
+
+# display the FFT array in a power spectrum
+ImageJFunctions.show(fft_img).setTitle("FFT power spectrum")
+
+# get a cursor on the FFT array
+fft_cursor = fft_img.cursor()
+
+# note dimension order is X, Y or col, row
+cursor_pos = [None, None]
+origin_a = [0, 0]
+origin_b = [0, fft_img.dimension(1)]
+origin_c = [fft_img.dimension(0), 0]
+origin_d = [fft_img.dimension(0), fft_img.dimension(1)]
+
+# loop through the FFT array
+while fft_cursor.hasNext():
+
+    # advance cursor and localize
+    fft_cursor.fwd()
+    cursor_pos[0] = fft_cursor.getLongPosition(0)
+    cursor_pos[1] = fft_cursor.getLongPosition(1)
+
+    # calculate distance from origins
+    dist_a = Util.distance(origin_a, cursor_pos)
+    dist_b = Util.distance(origin_b, cursor_pos)
+    dist_c = Util.distance(origin_c, cursor_pos)
+    dist_d = Util.distance(origin_d, cursor_pos)
+    
+    # Remove low frequences
+    if (dist_a <= radius) or \
+        (dist_b <= radius) or \
+        (dist_c <= radius) or \
+        (dist_d <= radius):
+        fft_cursor.get().setZero()
+
+# show FFT array after filter
+ImageJFunctions.show(fft_img).setTitle("fft after filter")
+
+# reconstruct filtered image from FFT array and show
+recon = ops.create().img([img.dimension(0), img.dimension(1)])
+ops.filter().ifft(recon, fft_img)

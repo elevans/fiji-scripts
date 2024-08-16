d4b97383e99573290b782f456ea83ecd00ac74dd
Edward Evans <elevans2@wisc.edu>
2024-08-16 11:05:51 -0500

    Add imglib2 mesh to 3D viewer script

---
+++ scijava-ops/mesh/view_mesh.py
@@ -0,0 +1,73 @@
+#@ OpEnvironment ops
+#@ ImgPlus img
+#@ Float (label = "Isolevel", style = "format:0.00", min = 1.0, value = 1.0) isolevel
+
+from net.imglib2.type.logic import BitType
+from net.imglib2.type.numeric.integer import ByteType
+from net.imglib2.type.numeric.integer import UnsignedShortType
+from net.imglib2.type.numeric.real import FloatType
+
+from org.scijava.vecmath import Point3f
+
+from customnode import CustomTriangleMesh
+from ij3d import Image3DUniverse
+
+def apply_isolevel(image, isolevel):
+    """Apply the desired isolevel on the input image.
+
+    Apply a desired isolevel (i.e. isosurface) value on the input image,
+    returning a BitType image that can be used with the marching cubes Op.
+
+    :param image:
+
+        Input ImgPlus.
+
+    :param isolevel:
+
+        Input isolevel value (float).
+
+    :return:
+
+        Output ImgPlus of BitType at desired isolevel.
+
+    """
+    # get the first element to determine how to convert isolevel value
+    fe = image.firstElement()
+    if isinstance(fe, ByteType):
+        val = ByteType(int(isolevel))
+    elif isinstance(fe, FloatType):
+        val = FloatType(isolevel)
+    elif isinstance(fe, UnsignedShortType):
+        val = UnsignedShortType(int(isolevel))
+    else:
+        val = None
+
+    out = ops.op("create.img").input(image, BitType()).apply()
+    ops.op("threshold.apply").input(image, val).output(out).compute()
+    
+    return out
+
+
+def view_mesh(image):
+    """Create an ImgLib2 mesh and view in the 3D viewer.
+
+    Convert the input ImgPlus BitType image into an ImgLib2 mesh
+    and display it with the 3D viewer.
+
+    :param image:
+
+        Input ImgPlus of BitType.
+    """
+    mesh = ops.op("geom.marchingCubes").input(image).apply()
+    points = []
+    for t in mesh.triangles():
+        points.append(Point3f(t.v0xf(), t.v0yf(), t.v0zf()))
+        points.append(Point3f(t.v1xf(), t.v1yf(), t.v1zf()))
+        points.append(Point3f(t.v2xf(), t.v2yf(), t.v2zf()))
+
+    ctm = CustomTriangleMesh(points)
+    univ = Image3DUniverse()
+    univ.addCustomMesh(ctm, "mesh")
+    univ.show()
+
+view_mesh(apply_isolevel(img, isolevel))

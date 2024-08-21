031ee03d6a0e1463ed8ccb3ec3ae83f58cfed6b9
Edward Evans <elevans2@wisc.edu>
2024-08-21 10:24:30 -0500

    Add "Global" to the threshold label

--- scijava-ops/threshold/apply_global_threshold.py
+++ scijava-ops/threshold/apply_global_threshold.py
@@ -1,7 +1,7 @@
 #@ OpEnvironment ops
 #@ UIService ui
 #@ Img (label="Input image:", autofill=false) img
-#@ String (label="Threshold method:", choices={"huang", "ij1", "intermodes", "isoData", "li", "maxEntropy", "maxLikelihood", "mean", "minError", "minimum", "moments", "otsu", "percentile", "renyiEntropy", "rosin", "shanbhag", "triangle", "yen"}, style="listBox") method
+#@ String (label="Global threshold method:", choices={"huang", "ij1", "intermodes", "isoData", "li", "maxEntropy", "maxLikelihood", "mean", "minError", "minimum", "moments", "otsu", "percentile", "renyiEntropy", "rosin", "shanbhag", "triangle", "yen"}, style="listBox") method
 
 from net.imglib2.type.logic import BitType
 

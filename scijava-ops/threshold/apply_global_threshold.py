4fa42b491fe42a4fb079113614f7f8a365ef9470
Edward Evans <elevans2@wisc.edu>
2024-08-21 09:30:41 -0500

    Add global threshold python script

---
+++ scijava-ops/threshold/apply_global_threshold.py
@@ -0,0 +1,10 @@
+#@ OpEnvironment ops
+#@ UIService ui
+#@ Img (label="Input image:", autofill=false) img
+#@ String (label="Threshold method:", choices={"huang", "ij1", "intermodes", "isoData", "li", "maxEntropy", "maxLikelihood", "mean", "minError", "minimum", "moments", "otsu", "percentile", "renyiEntropy", "rosin", "shanbhag", "triangle", "yen"}, style="listBox") method
+
+from net.imglib2.type.logic import BitType
+
+out = ops.op("create.img").input(img, BitType()).apply()
+ops.op("threshold.{}".format(method)).input(img).output(out).compute()
+ui.show("{} threshold output".format(method), out)

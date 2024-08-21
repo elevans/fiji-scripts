a436f26be5b36fb80016ac7ece82fddb4afaf418
Edward Evans <elevans2@wisc.edu>
2024-08-20 20:44:48 -0500

    Add transform hypersliceview script

---
+++ scijava-ops/transform/img_hyper_slice.py
@@ -0,0 +1,7 @@
+#@ OpEnvironment ops
+#@ Img (autofill=false) img
+#@ Integer (label="Dimension", min=1, value=1) dim
+#@ Integer (label="Position,", min=1, value=1) pos
+#@output Img output
+
+output = ops.op("transform.hyperSliceView").input(img, dim - 1, pos -  1).apply()

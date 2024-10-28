d89ff00b6f5a6085fdd5f021bc6a94d9893c6f98
Edward Evans <elevans2@wisc.edu>
2024-10-28 10:38:38 -0500

    Convert decon groovy script to jython

--- imagej2/deconvolution/ijo_rtlv_deconvolution.py
+++ imagej2/deconvolution/ijo_rtlv_deconvolution.py
@@ -1,37 +1,35 @@
-#@ ImgPlus img
 #@ OpService ops
+#@ UIService ui
+#@ Img (label = "Input image:", autofill = false) img
 #@ Integer iterations(label="Iterations", value=15)
 #@ Float numericalAperture(label="Numerical Aperture", style="format:0.00", min=0.00, value=1.45)
-#@ Integer wavelength(label="Emission Wavelength (nm)", value=550)
+#@ Integer wavelength(label="Emission Wavelength (nm)", value=457)
 #@ Float riImmersion(label="Refractive Index (immersion)", style="format:0.00", min=0.00, value=1.5)
 #@ Float riSample(label="Refractive Index (sample)", style="format:0.00", min=0.00, value=1.4)
 #@ Float lateral_res(label="Lateral resolution (μm/pixel)", style="format:0.0000", min=0.0000, value=0.065)
 #@ Float axial_res(label="Axial resolution (μm/pixel)", style="format:0.0000", min=0.0000, value=0.1)
 #@ Float pZ(label="Particle/sample Position (μm)", style="format:0.0000", min=0.0000, value=0)
 #@ Float regularizationFactor(label="Regularization factor", style="format:0.00000", min=0.00000, value=0.002)
-#@output ImgPlus psf
-#@output ImgPlus result
+#@ Boolean (label = "Show PSF:", value = false) show_psf
+#@output Img result
 
-import ij.IJ
-import net.imglib2.FinalDimensions
-import net.imglib2.type.numeric.real.FloatType
+from net.imglib2 import FinalDimensions
+from net.imglib2.type.numeric.real import FloatType
+from net.imglib2.type.numeric.complex import ComplexFloatType
 
-// convert interger parameters to float
-wavelength = wavelength.toFloat()
+# convert input image to float
+img = ops.convert().float32(img)
 
-// convert input image to 32-bit
-img_f = ops.convert().float32(img)
+# use image dimensions for PSF size
+psf_size = FinalDimensions(img.dimensionsAsLongArray())
 
-// get imglib2 dimensions from the input image
-psf_size = new FinalDimensions(img.dimensionsAsLongArray())
-
-// convert the input parameters to meteres (m)
-wavelength = wavelength * 1E-9
+# convert the input parameters to meters (m)
+wavelength = float(wavelength) * 1E-9
 lateral_res = lateral_res * 1E-6
 axial_res = axial_res * 1E-6
 pZ = pZ * 1E-6
 
-// create the synthetic PSF
+# create the synthetic PSF
 psf = ops.create().kernelDiffraction(
     psf_size,
     numericalAperture,
@@ -41,8 +39,12 @@ psf = ops.create().kernelDiffraction(
     lateral_res,
     axial_res,
     pZ,
-    new FloatType()
+    FloatType()
     )
 
-// deconvolve image
-result = ops.deconvolve().richardsonLucyTV(img_f, psf, iterations, regularizationFactor)
+# deconvolve image
+result = ops.deconvolve().richardsonLucyTV(img, psf, iterations, regularizationFactor)
+
+# optionally show the PSF
+if show_psf:
+    ui.show(psf)
